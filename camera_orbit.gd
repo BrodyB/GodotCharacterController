79d2fe72f321963a8bf73d591e09bbf49f8ca20e
Brody Brooks <brody@brodybrooks.com>
2024-05-07 22:08:39 -0700

    camera_orbit: added tweakable options, simplified rotation/pivot setup

--- camera_orbit.gd
+++ camera_orbit.gd
@@ -1,23 +1,28 @@
 class_name CameraOrbit3D
 extends Node3D
 
-const MOUSE_SENS : float = 0.25
+@export_category("Mouse Settings")
+@export var mouse_sensitivity : Vector2 = Vector2(3.0, 3.0)
 
-@onready var pivot_vert = $"Pivot Vert"
+@export_category("Orbit Settings")
+@export var pitch_limit : Vector2 = Vector2(-90, 90)
+@export var distance : float = 3.0
+@export var lateral_offset : float = 0.0
+
+@onready var pivot : Node3D = get_parent()
+var mouse_input : Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	pass # Replace with function body.
+	position = Vector3(lateral_offset, 0, distance)
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		rotate_y(deg_to_rad(-event.relative.x * MOUSE_SENS))
-		
-		pivot_vert.rotate_x(deg_to_rad(-event.relative.y * MOUSE_SENS))
-		pivot_vert.rotation.x = clamp(pivot_vert.rotation.x, deg_to_rad(-90), deg_to_rad(90))
-		
-		rotation.z = 0
+		pivot.rotation.x -= event.relative.y * mouse_sensitivity.y * 0.001
+		pivot.rotation.x = clamp(pivot.rotation.x, deg_to_rad(pitch_limit.x), deg_to_rad(pitch_limit.y))
+		pivot.rotation.y -= event.relative.x * mouse_sensitivity.x * 0.001
+		mouse_input = event.relative
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
