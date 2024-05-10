0c7cd42c511583d62f3f429bf89dedaf0597065d
Brody Brooks <brody@brodybrooks.com>
2024-05-09 20:05:01 -0700

    camera_orbit: First pass of collision detection (still jitters, though)

--- camera_orbit.gd
+++ camera_orbit.gd
@@ -11,11 +11,19 @@ extends Node3D
 
 @onready var pivot : Node3D = get_parent()
 var mouse_input : Vector2
+var collider : CollisionShape3D
+var desired_position : Vector3
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	position = Vector3(lateral_offset, 0, distance)
+	desired_position = position
+	
+	var sphere = SphereShape3D.new()
+	sphere.radius = 0.5
+	collider = CollisionShape3D.new()
+	collider.shape = sphere
 
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -23,7 +31,30 @@ func _input(event):
 		pivot.rotation.x = clamp(pivot.rotation.x, deg_to_rad(pitch_limit.x), deg_to_rad(pitch_limit.y))
 		pivot.rotation.y -= event.relative.x * mouse_sensitivity.x * 0.001
 		mouse_input = event.relative
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+		
 func _process(delta):
-	pass
+	DebugDraw3D.draw_sphere(global_position, 0.6, Color.RED)
+	position = position.lerp(desired_position, delta * 8)
+		
+func _physics_process(delta):
+	var space_state = get_world_3d().direct_space_state
+	
+	desired_position.z = distance
+	
+	var shape_cast = PhysicsShapeQueryParameters3D.new()
+	shape_cast.collide_with_areas = false
+	shape_cast.collide_with_bodies = true
+	shape_cast.shape = SphereShape3D.new()
+	shape_cast.shape.radius = 0.6
+	shape_cast.transform.origin = global_position
+	shape_cast.exclude = [$"../.."]
+	
+	var result = space_state.intersect_shape(shape_cast)
+
+	if result:
+		var line_cast = PhysicsRayQueryParameters3D.create(pivot.global_position, to_global(desired_position))
+		line_cast.exclude = [$"../.."]
+		
+		var hit = space_state.intersect_ray(line_cast)
+		if hit:
+			desired_position.z = to_local(hit.position).z - 0.6
